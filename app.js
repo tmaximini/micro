b3bf78118b238428bd847e6e47033a56a29d1639
Thomas Maximini <thomas.maximini.extern@zalando.de>
2016-06-20 14:21:40 +0200

    use api design from swagger specification

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const redis = new Redis();
 const app = new Koa();
 const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser');
+const uuid = require('node-uuid');
+
+function errorLogger(error, next) {
+  console.error('error', err);
+  next(new Error(err));
+}
 
 async function responseTime(ctx, next) {
   const start = new Date();
@@ -19,7 +25,7 @@ function logger(format) {
   return async function(ctx, next) {
     const str = format
       .replace(':method', ctx.method)
-      .replace(':url', ctx.url);
+      .replace(':url', ctx.url)
 
     console.log(str);
 
@@ -31,6 +37,7 @@ async function errorHandler(ctx, next) {
   try {
     await next(); // next is now a function
   } catch (err) {
+    console.log('an error occured', err.message);
     ctx.body = { message: err.message };
     ctx.status = err.status || 500;
   }
@@ -38,40 +45,100 @@ async function errorHandler(ctx, next) {
 
 // middleware
 app.use(responseTime);
+app.use(errorHandler);
 app.use(logger());
 app.use(bodyParser());
-app.use(errorHandler);
 
 
 // returns all messages for a uuid and deletes them afterwards
-async function popAllMessagesForUser(uuid) {
+async function popAllMessagesForUser(clientId) {
   const messages = [];
-  const length = await redis.llen(uuid);
+  const length = await redis.llen(clientId);
   for (var i = length - 1; i >= 0; i--) {
-    let msg = await redis.lpop(uuid);
+    let msg = await redis.lpop(clientId);
     messages.push(JSON.parse(msg));
   }
   return messages;
 }
 
+// returns all messages without deleting them
+async function getAllMessagesForUser(clientId, offset, limit) {
+  return redis.lrange(clientId, offset, limit);
+}
+
+async function getMessageById(clientId, messageId, options) {
+  const length = await redis.llen(clientId);
+  for (var i = length - 1; i >= 0; i--) {
+    const el = await redis.lrange(clientId, i, 1);
+    const parsed = JSON.parse(el);
+    console.log('parsed', parsed);
+    if (parsed.messageId === messageId) {
+      if (options && options.delete) {
+
+      }
+      return parsed;
+    }
+  }
+  return null;
+}
+
 
 router
-  .get('/:uuid', async (ctx, next) => {
+  // gets all messages
+  .get('/clients/:clientId/messages', async (ctx, next) => {
     try {
-      const messages = await popAllMessagesForUser(ctx.params.uuid);
+      const offset = ctx.query.offset || 0;
+      const limit = ctx.query.limit || -1;
+      const result = await getAllMessagesForUser(ctx.params.clientId, offset, limit);
+      const messages = JSON.parse(result);
       ctx.body = { messages };
     }
     catch (err) {
-      next(new Error(err));
+      errorLogger(err, next);
     }
   })
-  .post('/:uuid', (ctx, next) => {
+  // gets a single message
+  .get('/clients/:clientId/messages/:messageId', async (ctx, next) => {
     try {
-      const body = ctx.request.body;
-      redis.lpush(ctx.params.uuid, JSON.stringify(body.message));
+      const message = await getMessageById(ctx.params.clientId, ctx.params.messageId);
+      ctx.status = 200;
+      ctx.body = { message };
     }
     catch (err) {
-      next(new Error(err))
+      errorLogger(err, next);
+    }
+  })
+  // creates a new message
+  .post('/clients/:clientId/messages', (ctx, next) => {
+    try {
+      const body = ctx.request.body;
+      // if no messageId is present, generate one
+      body.messageId = body.messageId || uuid.v4();
+      redis.lpush(ctx.params.clientId, JSON.stringify(body));
+      ctx.status = 200;
+    }
+    catch (err) {
+      errorLogger(err, next);
+    }
+  })
+  // deletes all messages
+  .del('/clients/:clientId/messages', (ctx, next) => {
+    try {
+      redis.del(ctx.params.clientId);
+      ctx.status = 200;
+    }
+    catch (err) {
+      errorLogger(err, next);
+    }
+  })
+  // deletes a single message
+  .del('/clients/:clientId/messages/:messageId', (ctx, next) => {
+    try {
+      getMessageById(ctx.params.clientId, ctx.params.messageId, { delete: true });
+      ctx.status = 200;
+    }
+    catch (err) {
+      errorLogger(err, next);
     }
   });
 
